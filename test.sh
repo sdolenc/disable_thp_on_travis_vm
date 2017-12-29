e28503c36631ead7de77d3f49e668376ff15ead9
x3v16stepdo1 <you@example.com>
2017-12-29 11:25:04 -0800

    url references

--- test.sh
+++ test.sh
@@ -3,6 +3,10 @@
 
 set -e
 
+# From 
+# https://askubuntu.com/questions/597372/how-do-i-modify-sys-kernel-mm-transparent-hugepage-enabled
+# https://forums.docker.com/t/disable-transparent-huge-pages-thp/17240
+
 echo never >/sys/kernel/mm/transparent_hugepage/enabled 
 
 echo never >/sys/kernel/mm/transparent_hugepage/defrag
